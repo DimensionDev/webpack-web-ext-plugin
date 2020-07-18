51a6bef3fd452169ee711ad5ffd3c06af6f82096
Septs <github@septs.pw>
2020-07-18 18:26:07 +0800

    automate publish to github package

--- index.js
+++ index.js
@@ -1,90 +1,125 @@
-const webExt = require('web-ext').default
-const inquirer = require('inquirer')
+const webExt = require("web-ext").default;
+const inquirer = require("inquirer");
 
-const pluginName = 'web-ext-plugin'
+const pluginName = "web-ext-plugin";
 
 class WebExtWebpackPlugin {
-  constructor (CmdRunParams = {}, CmdRunOptions = {}, PluginOptions = {}) {
-    this.logger = console.log.bind(console)
-    this.CmdRunParams = { ...CmdRunParams, noReload: true, noInput: true }
-    this.CmdRunOptions = { ...CmdRunOptions, shouldExitProgram: false }
-    this.verbose = PluginOptions.verbose || false
-    this.timeout = PluginOptions.timeout || 5000
+  constructor(CmdRunParams = {}, CmdRunOptions = {}, PluginOptions = {}) {
+    this.logger = console.log.bind(console);
+    this.CmdRunParams = { ...CmdRunParams, noReload: true, noInput: true };
+    this.CmdRunOptions = { ...CmdRunOptions, shouldExitProgram: false };
+    this.verbose = PluginOptions.verbose || false;
+    this.timeout = PluginOptions.timeout || 5000;
   }
 
-  androidSetup () {
-    let shouldTimeoutReject = true
+  androidSetup() {
+    let shouldTimeoutReject = true;
     this.androidSetupPromise = new Promise((resolve, reject) => {
-      setTimeout(() => shouldTimeoutReject && reject(new Error('No android device found.')), this.timeout)
+      setTimeout(
+        () =>
+          shouldTimeoutReject && reject(new Error("No android device found.")),
+        this.timeout
+      );
       webExt.util.logger.consoleStream.write = (...args) => {
-        const raw = args[0].msg
-        const message = (raw || '').trim().split('\n')
-        if (message[0] !== 'Android devices found:') { return this.verbose && this.logger(raw) }
-        shouldTimeoutReject = false
-        const devices = message.slice(1).map(s => s.substr(3))
-        if (devices.length === 1) {
-          this.logger('Selecting the only device ' + devices[0])
-          this.CmdRunParams.adbDevice = devices[0]
-          return resolve()
+        const raw = args[0].msg;
+        const message = (raw || "").trim().split("\n");
+        if (message[0] !== "Android devices found:") {
+          return this.verbose && this.logger(raw);
         }
-        inquirer.prompt({ type: 'list', choices: devices, name: 'device', message: 'Select one device to continue' }).then(answer => {
-          webExt.util.logger.consoleStream.write = e => this.verbose && this.logger(e)
-          this.CmdRunParams.adbDevice = answer.device
-        }).then(resolve)
-      }
-    })
+        shouldTimeoutReject = false;
+        const devices = message.slice(1).map((s) => s.substr(3));
+        if (devices.length === 1) {
+          this.logger("Selecting the only device " + devices[0]);
+          this.CmdRunParams.adbDevice = devices[0];
+          return resolve();
+        }
+        inquirer
+          .prompt({
+            type: "list",
+            choices: devices,
+            name: "device",
+            message: "Select one device to continue",
+          })
+          .then((answer) => {
+            webExt.util.logger.consoleStream.write = (e) =>
+              this.verbose && this.logger(e);
+            this.CmdRunParams.adbDevice = answer.device;
+          })
+          .then(resolve);
+      };
+    });
   }
 
-  async androidQueryDevice () {
-    if (this.CmdRunParams.adbDevice) return
-    await webExt.cmd.run(this.CmdRunParams, this.CmdRunOptions).catch(() => {})
-    await this.androidSetupPromise.catch(e => {
-      this.error(e)
-      throw e
-    })
+  async androidQueryDevice() {
+    if (this.CmdRunParams.adbDevice) return;
+    await webExt.cmd.run(this.CmdRunParams, this.CmdRunOptions).catch(() => {});
+    await this.androidSetupPromise.catch((e) => {
+      this.error(e);
+      throw e;
+    });
   }
 
-  async watchRun () {
-    this.watchMode = true
+  async watchRun() {
+    this.watchMode = true;
   }
 
-  async afterEmit () {
-    if (!this.watchMode) return
+  async afterEmit() {
+    if (!this.watchMode) return;
 
     if (this.runner) {
-      this.logger('Webpack emitted, reloading...')
-      let reloadOK = await Promise.race([this.runner.reloadAllExtensions().then(() => true).catch(this.error), new Promise(resolve => setTimeout(resolve, this.timeout, false))])
+      this.logger("Webpack emitted, reloading...");
+      let reloadOK = await Promise.race([
+        this.runner
+          .reloadAllExtensions()
+          .then(() => true)
+          .catch(this.error),
+        new Promise((resolve) => setTimeout(resolve, this.timeout, false)),
+      ]);
       if (reloadOK) {
-        this.logger('Extension reloaded.')
-        return
+        this.logger("Extension reloaded.");
+        return;
       }
     }
 
-    const runner = await webExt.cmd.run(this.CmdRunParams, this.CmdRunOptions).catch(this.error)
-    this.runner = runner
+    const runner = await webExt.cmd
+      .run(this.CmdRunParams, this.CmdRunOptions)
+      .catch(this.error);
+    this.runner = runner;
 
     // Fix web-ext's bad
-    process.stdin.setRawMode(false)
+    process.stdin.setRawMode(false);
 
     runner.registerCleanup(() => {
-      this.runner = null
-    })
+      this.runner = null;
+    });
   }
 
-  apply (compiler) {
-    const logger = compiler.getInfrastructureLogger(pluginName)
-    this.logger = logger.info.bind(logger)
-    this.error = logger.error.bind(logger)
+  apply(compiler) {
+    const logger = compiler.getInfrastructureLogger(pluginName);
+    this.logger = logger.info.bind(logger);
+    this.error = logger.error.bind(logger);
 
-    if (this.CmdRunParams.target === 'firefox-android' && !this.CmdRunParams.adbDevice) {
-      this.androidSetup.bind(this)()
-      this.androidQueryDeviceResult = this.androidQueryDevice.bind(this)()
-      compiler.hooks.beforeCompile.tapPromise({ name: pluginName }, () => this.androidQueryDeviceResult)
+    if (
+      this.CmdRunParams.target === "firefox-android" &&
+      !this.CmdRunParams.adbDevice
+    ) {
+      this.androidSetup.bind(this)();
+      this.androidQueryDeviceResult = this.androidQueryDevice.bind(this)();
+      compiler.hooks.beforeCompile.tapPromise(
+        { name: pluginName },
+        () => this.androidQueryDeviceResult
+      );
     }
 
-    compiler.hooks.done.tapPromise({ name: pluginName }, this.afterEmit.bind(this))
-    compiler.hooks.watchRun.tapPromise({ name: pluginName }, this.watchRun.bind(this))
+    compiler.hooks.done.tapPromise(
+      { name: pluginName },
+      this.afterEmit.bind(this)
+    );
+    compiler.hooks.watchRun.tapPromise(
+      { name: pluginName },
+      this.watchRun.bind(this)
+    );
   }
 }
 
-module.exports = WebExtWebpackPlugin
+module.exports = WebExtWebpackPlugin;
